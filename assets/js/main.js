142990e92a3a0f53063f7586ebce5f2ae9276ecb
rpgavr <71153474+rpgavr@users.noreply.github.com>
2023-02-14 00:24:49 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,29 @@
 		});
 
 })(jQuery);
+
+function jogada() {
+        var jogadaJogador = Math.floor(Math.random() * 6) + 1;
+        var jogadaIA = Math.floor(Math.random() * 6) + 1;
+        var resultado = document.getElementById("resultado");
+
+        if (jogadaJogador === 1 || jogadaJogador === 2 || jogadaJogador === 3) {
+          resultado.innerHTML = "A ação foi " + jogadaJogador + " e falhou";
+        
+        } else if (jogadaJogador === 6) {
+          resultado.innerHTML = "A ação foi " + jogadaJogador + " e acertou com um crítico!";
+          
+        } else {
+          resultado.innerHTML = "A ação foi " + jogadaJogador + " e acertou.";
+          if (jogadaIA === 1 || jogadaIA === 2 || jogadaIA === 3) {
+            resultado.innerHTML += "<br>A reação foi " + jogadaIA + " e falhou.";
+            
+          } else if (jogadaIA === 6) {
+            resultado.innerHTML += "<br>A reação foi " + jogadaIA + "  defendeu com um crítico!";
+            
+          } else {
+            resultado.innerHTML += "<br>A reação foi " + jogadaIA + " e defendeu.";
+          }
+        }
+
+      }
